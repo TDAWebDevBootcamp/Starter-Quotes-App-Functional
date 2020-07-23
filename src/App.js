0b1bda06e63355a256f429bd68e85f4f199a4ff7
N Bizzell <bizzbizzell@gmail.com>
2020-07-23 16:33:48 +0100

    Created starter code

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      content: "",
+      author: "",
+      tags: []
+    }
+  }
+
+  componentDidMount(){
+    this.setState({
+      content: "a quote we made up",
+      author: "anon",
+      tags: ["quote", "stuff", "blah"]
+    })  
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Quote of the day</h1>
+        <p><b>Content:</b> {this.state.content} </p>
+        <p><b>Author:</b> {this.state.author} </p>
+        <p><b>Tags:</b> {this.state.tags.join(", ")}</p>
+      </>
+    )
+  }
+}
+
+export default App;
